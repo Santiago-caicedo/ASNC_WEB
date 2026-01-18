1008393c2afb0e8062a198325fd22b6601dcd611
Santiago Caicedo <scaicedo751@unab.edu.co>
2026-01-17 19:27:06 -0500

    seo tecnico implementado

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings # Importar settings
-from django.conf.urls.static import static # Importar static
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
+
+from website.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # SEO: Sitemap y robots.txt
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots'),
+
     # App Website (La raíz del sitio)
     path('', include('website.urls')),
     path('', include('admissions.urls')),
---
+++ website/sitemaps.py
@@ -0,0 +1,36 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+
+class StaticViewSitemap(Sitemap):
+    """Sitemap para páginas estáticas del sitio público"""
+    priority = 0.8
+    changefreq = 'weekly'
+    protocol = 'https'
+
+    def items(self):
+        return ['home', 'about', 'events']
+
+    def location(self, item):
+        return reverse(item)
+
+    def priority(self, item):
+        priorities = {
+            'home': 1.0,
+            'about': 0.8,
+            'events': 0.7,
+        }
+        return priorities.get(item, 0.5)
+
+
+class HomeSitemap(Sitemap):
+    """Sitemap específico para la página principal con mayor prioridad"""
+    priority = 1.0
+    changefreq = 'daily'
+    protocol = 'https'
+
+    def items(self):
+        return ['home']
+
+    def location(self, item):
+        return reverse(item)
